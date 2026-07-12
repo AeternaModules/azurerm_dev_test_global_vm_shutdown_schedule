79c2860acf436d9d9c19b5d40382c27bdcdb91bf
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "dev_test_global_vm_shutdown_schedules" {
-  description = "All dev_test_global_vm_shutdown_schedule resources"
-  value       = azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules
-}
 output "dev_test_global_vm_shutdown_schedules_daily_recurrence_time" {
-  description = "List of daily_recurrence_time values across all dev_test_global_vm_shutdown_schedules"
-  value       = [for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : v.daily_recurrence_time]
+  description = "Map of daily_recurrence_time values across all dev_test_global_vm_shutdown_schedules, keyed the same as var.dev_test_global_vm_shutdown_schedules"
+  value       = { for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : k => v.daily_recurrence_time }
 }
 output "dev_test_global_vm_shutdown_schedules_enabled" {
-  description = "List of enabled values across all dev_test_global_vm_shutdown_schedules"
-  value       = [for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : v.enabled]
+  description = "Map of enabled values across all dev_test_global_vm_shutdown_schedules, keyed the same as var.dev_test_global_vm_shutdown_schedules"
+  value       = { for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : k => v.enabled }
 }
 output "dev_test_global_vm_shutdown_schedules_location" {
-  description = "List of location values across all dev_test_global_vm_shutdown_schedules"
-  value       = [for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : v.location]
+  description = "Map of location values across all dev_test_global_vm_shutdown_schedules, keyed the same as var.dev_test_global_vm_shutdown_schedules"
+  value       = { for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : k => v.location }
 }
 output "dev_test_global_vm_shutdown_schedules_notification_settings" {
-  description = "List of notification_settings values across all dev_test_global_vm_shutdown_schedules"
-  value       = [for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : v.notification_settings]
+  description = "Map of notification_settings values across all dev_test_global_vm_shutdown_schedules, keyed the same as var.dev_test_global_vm_shutdown_schedules"
+  value       = { for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : k => v.notification_settings }
 }
 output "dev_test_global_vm_shutdown_schedules_tags" {
-  description = "List of tags values across all dev_test_global_vm_shutdown_schedules"
-  value       = [for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : v.tags]
+  description = "Map of tags values across all dev_test_global_vm_shutdown_schedules, keyed the same as var.dev_test_global_vm_shutdown_schedules"
+  value       = { for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : k => v.tags }
 }
 output "dev_test_global_vm_shutdown_schedules_timezone" {
-  description = "List of timezone values across all dev_test_global_vm_shutdown_schedules"
-  value       = [for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : v.timezone]
+  description = "Map of timezone values across all dev_test_global_vm_shutdown_schedules, keyed the same as var.dev_test_global_vm_shutdown_schedules"
+  value       = { for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : k => v.timezone }
 }
 output "dev_test_global_vm_shutdown_schedules_virtual_machine_id" {
-  description = "List of virtual_machine_id values across all dev_test_global_vm_shutdown_schedules"
-  value       = [for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : v.virtual_machine_id]
+  description = "Map of virtual_machine_id values across all dev_test_global_vm_shutdown_schedules, keyed the same as var.dev_test_global_vm_shutdown_schedules"
+  value       = { for k, v in azurerm_dev_test_global_vm_shutdown_schedule.dev_test_global_vm_shutdown_schedules : k => v.virtual_machine_id }
 }
 
